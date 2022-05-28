b891164fd48096b3465bc4b3a5cba449121eea67
daniela <danielavento25@gmail.com>
2022-05-28 15:44:59 -0400

    new advance

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 import runCommandLine from "./mmm.js";
 import promptFact from 'prompt-sync'
- 
+
 const prompt = promptFact()
-
-const products= ['sal, azucar, canela, harina, arroz']
-
-const productLast= ['sal, canela, harina, arroz']
+    
+  products={
+    name:['cafe','arroz','azucar','leche'],
+    cantidad:[10, 15, 30, 25],
+    UM:['lbs','kg', 'lbs', 'lts'],
+    precio:['$15', '$30', '$18','$25'],
+  }
 
 runCommandLine(function (command){
+   
   if(command == 'add'){
     var productsName = prompt('Especifique el nombre del producto: ');
-  products.push(productsName);
+    products.push(productsName);
   }
+  
   else if(command == 'show'){
-     console.log(products);
+    console.log(products);
   }
+  
+  else if(command == 'modify'){
+    var products=prompt('Especifique el producto que desea modificar: ');
+    someProduct.price='$30';
+  }
+
+  
+  else if(command == 'list'){
+    console.log(products)
+  }
+
   else if(command == 'delete'){
- var product= prompt('Especifique el elemento que desea eliminar: ');
-   console.log(productLast); 
- }
+    var products= prompt('Especifique el elemento que desea eliminar: ');
+  }
+  
   else{
-    console.log('el commando es: ' +command);
-}
+   console.log('el commando es: ' +command);
+  }
 });
